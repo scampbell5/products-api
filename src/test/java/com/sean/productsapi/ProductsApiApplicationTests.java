a64c150c30ac6d3adfec247c1c40e293f4152505
Sean Campbell <sean@Seans-Air.maine.rr.com>
2018-04-23 18:39:22 -0400

    Initial commit.

---
+++ src/test/java/com/sean/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sean.productsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
