86f02ab9c6e209b64113f5656abaf8bcaa046c4b
Sean <sean.campbell1@maine.edu>
2018-04-24 19:24:43 -0400

    Created a ProductController that either returns all results, or searches on a given searchTerm.

---
+++ src/main/java/com/sean/productsapi/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.sean.productsapi.controller;
+
+import com.sean.productsapi.jpa.model.Product;
+import com.sean.productsapi.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts(@RequestParam(value = "searchTerm", required = false) String searchTerm) {
+
+        if (searchTerm == null) {
+            return productService.getAllProducts();
+        }
+
+        return productService.getProductsBySearchTermIgnoreCase(searchTerm);
+    }
+}
