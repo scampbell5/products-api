d58bc7cbc6c2b30207f8c57568e2eeebe97fda29
Sean <sean.campbell1@maine.edu>
2018-04-24 19:22:23 -0400

    Added a query to find products by a given search term, currently allows matching on department name, description and external id.

--- src/main/java/com/sean/productsapi/jpa/repository/ProductRepository.java
+++ src/main/java/com/sean/productsapi/jpa/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -13,4 +14,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
            " WHERE product.description = :description ")
     Optional<Product> findByDescription(@Param("description") String description);
 
+    @Query(" SELECT product FROM Product product " +
+           " WHERE lower(product.department.name) like %:searchTerm% or " +
+           " lower(product.description) like %:searchTerm% " +
+           " or product.externalId like %:searchTerm% ")
+    List<Product> findBySearchTermIgnoreCase(@Param("searchTerm") String searchTerm);
 }
