efc1a3c817a140b8d652846f535e78745de91184
Sean <sean.campbell1@maine.edu>
2018-04-25 22:11:21 -0400

    Set server.port to be 5000 when using demo profile, added origin for Firebase hosted app.

--- src/test/java/com/sean/productsapi/ProductsApiApplicationTests.java
+++ src/test/java/com/sean/productsapi/ProductsApiApplicationTests.java
@@ -3,8 +3,10 @@ package com.sean.productsapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("local")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductsApiApplicationTests {
